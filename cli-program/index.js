65b1348ac5c422c76702915a0fa3954f80b1c5b3
opendra-singh <oprack12346@gmail.com>
2025-09-02 23:21:12 +0530

    error in cli

--- cli-program/index.js
+++ cli-program/index.js
@@ -2,31 +2,41 @@ const {Command} = require('commander');
 const fs = require('fs');
 const program = new Command();
 
+
 program
-  .name('read file')
-  .description('CLI to read files and process the output')
+  .name('todo cli')
+  .description('CLI to perform CURD operation as a TODO')
   .version('1.0.0');
 
-program.command('countNumbers')
-  .description('count characters from file')
-  .argument('<path>', 'filename')
-  .option('-f', '--filename', 'a.txt')
-  .action((fileName) => {
-    const data = fs.readFile(fileName, 'UTF-8', function(err, data){
-        let count = data.split(' ');
-        console.log(count.length);
-    })    
-  });
-
-  program.command('upperCase')
-  .description('count characters from file')
-  .argument('<path>', 'filename')
-  .option('-f', '--filename', 'a.txt')
-  .action((fileName) => {
-    const data = fs.readFile(fileName, 'UTF-8', function(err, data){
-        let upperCase = data.toUpperCase();
-        console.log(upperCase);
-    })    
-  });
+  let data = {};
+  fs.readFile('data.json', 'utf-8', function(err, content) {
+    console.log("data");
+    
+    console.log(content);
+    if( err ) {
+        console.log("data not found");
+        
+    }
+    data = JSON.parse(data);
+  })
+  program.command('add_person')
+    .description('Add provided text to todo.json')
+    .argument('<string>', 'person name')
+    .argument('<number>', 'person age')
+    .option('-n', '--name <string>', 'Person name')
+    .option('-a', '--age <number>', 'Person age')
+    .action((name, age) => {
+        let object = {
+            'name' : name,
+            'age' : age
+        }
+      data['person'] = object;
+      let newData = JSON.stringify(data);
+      fs.writeFile('data.json', newData, function(err){
+        if(!err){
+            console.log("Person is added")
+        }
+      })
+    });
 
 program.parse();
