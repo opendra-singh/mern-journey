9615896e64844200d38bf08e1ed91bab358b1d76
opendra-singh <oprack12346@gmail.com>
2025-09-07 00:49:53 +0530

    todo done

--- todo-node/README.md
+++ todo-node/README.md
@@ -1,4 +1,10 @@
-This folder contains how i created first server using express</br>
-    1. Install express
-    2. use app.get to create route
-    3. use res.send to print onto the browser and postman followed by understanding the route
+This folder contains how i created a TODO CRUD on node and express</br>
+
+How i created the todo on node</br>
+    A. Installed express</br>
+    B. index.js used to perform all the commands and holds all routes, it can be run as(node index.js)</br>
+    C. Lets see into action</br>
+        a.) To add: type - send data on "/add/tasks" in "x-www-form-urlencoded", Params are: task and done_by </br>
+        a.) To update: type - send data on "/update/tasks" in "x-www-form-urlencoded", Params are: id, task and done_by </br>
+        a.) To fetch all: type - send data on "/delete/tasks" in "x-www-form-urlencoded", No params are required</br>
+        a.) To delete: type - send data on "/add/tasks" in "x-www-form-urlencoded", Param is: id</br>
--- todo-node/index.js
+++ todo-node/index.js
@@ -1,16 +1,80 @@
 const express = require("express");
+// const { json } = require("stream/consumers");
 const app = express();
+const fs = require('fs').promises;
 
-app.get("/", function(req, res){
-    res.send('Hello route initilized')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+async function readData() {
+    let sendData = [];
+    let data = await fs.readFile('data.json', 'UTF-8');
+    if( data != '' && data != undefined ) {
+        return JSON.parse(data);
+    }
+}
+
+async function writeData(content){
+    await fs.writeFile('data.json', JSON.stringify(content));
+}
+
+app.get("/", async function(req, res){
+
+    let arr = await readData();
+    // console.log(arr, 'arr')
+    // if(arr == undefined) res.send('No data yet to be sent');
+    let html = '';
+    arr.map((data) => {
+        html += 'Task is - ';
+        html += data.task;
+        html += ' and done by - ';
+        html += data.done_by;
+        html += '<br>';
+        html += '<br>';
+    })
+    // console.log(html);
+    res.send(html)
 })
 
-app.get("/tasks", function(req, res){
-    const tasks = {
-        "Node" : "Commander Cli, Chalk library, Http route etc",
-        "JS" : "Async await, Map, filter, promises, clousers, prototype and etc"
+app.post("/add/tasks", async function(req, res){
+    let bodyParams = req.body;
+    let arr = await readData();
+    // console.log(arr, 'opo');
+    let tasks = {
+        'id' : arr == null || undefined ? 1 : arr.length+1,
+        'task' : bodyParams.task,
+        'done_by' : bodyParams.done_by
     }
-    res.send(tasks);
+    // console.log(tasks, 'task');
+    let PushableData = [...(arr || []), tasks];
+    await writeData(PushableData);
+    res.send('Todo list Updated')
+})
+
+app.put("/update/tasks", async function(req, res){
+    let bodyParams = req.body;
+    let compareId = bodyParams.id;
+    let arr = await readData();
+    arr.map((data) => {
+        if( data.id == compareId ) {
+            data.task = bodyParams.task,
+            data.done_by = bodyParams.done_by
+        }
+    })
+    await writeData(arr);
+    // console.log(arr);
+    res.send('Todo entry Updated')
+})
+
+app.delete("/delete/tasks", async function(req, res){
+    let bodyParams = req.body;
+    let deleteId = bodyParams.id;
+    let arr = await readData();
+    arr = arr.filter((data) => {
+        return data.id != deleteId;
+    })
+    await writeData(arr);
+    res.send('entry deleted')
 })
 
 app.listen(3000);
